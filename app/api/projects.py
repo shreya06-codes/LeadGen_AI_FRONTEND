a280afca74284415db8d564585ef61bc69784284
sandhyapal1812 <sandhyapal6742@gmail.com>
2026-07-06 09:44:38 +0530

    Integrated login and crawler API

--- app/api/projects.py
+++ app/api/projects.py
@@ -1,69 +1,63 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from uuid import uuid4
 
-# Router
+from services.project_services import (
+    create_project,
+    get_projects,
+    get_project,
+    delete_project
+)
+
 router = APIRouter(
     prefix="/projects",
     tags=["Projects"]
 )
 
-# Temporary storage
-projects = []
 
-# Project Schema
 class ProjectCreate(BaseModel):
+
     name: str
     description: str
+    industry: str
+    country: str
 
-# Create Project
+
 @router.post("/")
-def create_project(project: ProjectCreate):
+def add_project(project: ProjectCreate):
 
-    new_project = {
-        "id": str(uuid4()),
-        "name": project.name,
-        "description": project.description
-    }
+    return create_project(project.model_dump())
 
-    projects.append(new_project)
 
-    return {
-        "message": "Project created successfully",
-        "project": new_project
-    }
-
-# Get All Projects
 @router.get("/")
-def get_projects():
+def all_projects():
+
     return {
-        "total_projects": len(projects),
-        "projects": projects
+        "total_projects": len(get_projects()),
+        "projects": get_projects()
     }
 
-# Get Single Project
+
 @router.get("/{project_id}")
-def get_project(project_id: str):
+def single_project(project_id: str):
 
-    for project in projects:
-        if project["id"] == project_id:
-            return project
+    project = get_project(project_id)
 
-    raise HTTPException(
-        status_code=404,
-        detail="Project not found"
-    )
+    if not project:
+        raise HTTPException(
+            status_code=404,
+            detail="Project not found"
+        )
 
-# Delete Project
+    return project
+
+
 @router.delete("/{project_id}")
-def delete_project(project_id: str):
+def remove_project(project_id: str):
 
-    for project in projects:
-        if project["id"] == project_id:
-            projects.remove(project)
-            return {
-                "message": "Project deleted successfully"
-            }
+    if delete_project(project_id):
+        return {
+            "message": "Project deleted successfully"
+        }
 
     raise HTTPException(
         status_code=404,
